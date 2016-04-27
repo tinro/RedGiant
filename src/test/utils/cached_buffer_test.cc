649195b64a0488a4112a1e8da37cedcef35a5144
Tinro <tinrow@gmail.com>
2016-04-27 15:27:53 +0800

    add more tests

--- src/test/utils/cached_buffer_test.cc
+++ src/test/utils/cached_buffer_test.cc
@@ -5,10 +5,27 @@
 
 namespace redgiant {
 
-class CachedBufferTest : public CppUnit::TestFixture
-{
+class MockBufferedItem {
+public:
+  static size_t alloc_count;
+  static size_t release_count;
+
+  MockBufferedItem() {
+    ++alloc_count;
+  }
+
+  ~MockBufferedItem() {
+    ++release_count;
+  }
+};
+
+size_t MockBufferedItem::alloc_count = 0;
+size_t MockBufferedItem::release_count = 0;
+
+class CachedBufferTest : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE(CachedBufferTest);
-  CPPUNIT_TEST(test_rebuffer);
+  CPPUNIT_TEST(test_size);
+  CPPUNIT_TEST(test_alloc);
   CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -16,21 +33,83 @@ public:
   virtual ~CachedBufferTest() = default;
 
 protected:
-  void test_rebuffer() {
-    CachedBuffer<int> buffer(4096);
+  void test_size() {
+    CachedBuffer<int> buffer(256);
     CPPUNIT_ASSERT_EQUAL(0, (int)buffer.size());
+    CPPUNIT_ASSERT(buffer.max_size() <= 256);
 
-    buffer.alloc(1024);
-    CPPUNIT_ASSERT_EQUAL(1024, (int)buffer.size());
+    buffer.alloc(100);
+    CPPUNIT_ASSERT_EQUAL(100, (int)buffer.size());
+    CPPUNIT_ASSERT(buffer.max_size() >= 100);
+    CPPUNIT_ASSERT(buffer.max_size() <= 256);
 
-    buffer.alloc(2048);
+    buffer.alloc(200);
     CPPUNIT_ASSERT_EQUAL(2048, (int)buffer.size());
-
-    buffer.alloc(8192);
-    CPPUNIT_ASSERT_EQUAL(8192, (int)buffer.size());
+    CPPUNIT_ASSERT(buffer.max_size() >= 200);
+    CPPUNIT_ASSERT(buffer.max_size() <= 256);
 
     buffer.clear();
     CPPUNIT_ASSERT_EQUAL(0, (int)buffer.size());
+    CPPUNIT_ASSERT(buffer.max_size() <= 256);
+
+    buffer.alloc(100);
+    CPPUNIT_ASSERT_EQUAL(100, (int)buffer.size());
+    CPPUNIT_ASSERT(buffer.max_size() >= 100);
+    CPPUNIT_ASSERT(buffer.max_size() <= 256);
+
+    buffer.alloc(300);
+    CPPUNIT_ASSERT_EQUAL(300, (int)buffer.size());
+    CPPUNIT_ASSERT(buffer.max_size() >= 300);
+
+    buffer.clear();
+    CPPUNIT_ASSERT_EQUAL(0, (int)buffer.size());
+    CPPUNIT_ASSERT(buffer.max_size() <= 256);
+
+    buffer.alloc(100);
+    CPPUNIT_ASSERT_EQUAL(100, (int)buffer.size());
+    CPPUNIT_ASSERT(buffer.max_size() >= 100);
+    CPPUNIT_ASSERT(buffer.max_size() <= 256);
+
+    buffer.clear();
+    CPPUNIT_ASSERT_EQUAL(0, (int)buffer.size());
+    CPPUNIT_ASSERT(buffer.max_size() <= 256);
+  }
+
+  void test_alloc() {
+    MockBufferedItem::alloc_count = 0;
+    MockBufferedItem::release_count = 0;
+
+    CachedBuffer<MockBufferedItem> *buffer = new CachedBuffer<MockBufferedItem>(256);
+    // not allocated by default
+    CPPUNIT_ASSERT_EQUAL(0, (int)MockBufferedItem::alloc_count);
+    CPPUNIT_ASSERT_EQUAL(0, (int)MockBufferedItem::release_count);
+
+    buffer->alloc(100);
+    // alloc (at least) 100 items
+    CPPUNIT_ASSERT_EQUAL(100, (int)buffer->size());
+    CPPUNIT_ASSERT(MockBufferedItem::alloc_count - MockBufferedItem::release_count >= 100);
+
+    buffer->alloc(200);
+    // alloc (at least) 200 items
+    CPPUNIT_ASSERT_EQUAL(200, (int)buffer->size());
+    CPPUNIT_ASSERT(MockBufferedItem::alloc_count - MockBufferedItem::release_count >= 200);
+
+    buffer->clear();
+    // released all items (except cached)
+    CPPUNIT_ASSERT_EQUAL(0, (int)buffer->size());
+    CPPUNIT_ASSERT(MockBufferedItem::alloc_count - MockBufferedItem::release_count >= 256);
+
+    buffer->alloc(300);
+    CPPUNIT_ASSERT_EQUAL(300, (int)buffer->size());
+    CPPUNIT_ASSERT(MockBufferedItem::alloc_count - MockBufferedItem::release_count >= 300);
+
+    buffer->clear();
+    CPPUNIT_ASSERT_EQUAL(0, (int)buffer->size());
+    CPPUNIT_ASSERT(MockBufferedItem::alloc_count - MockBufferedItem::release_count >= 256);
+
+    delete buffer;
+    // assert all memory are released
+    CPPUNIT_ASSERT_EQUAL(0, (int)(MockBufferedItem::alloc_count - MockBufferedItem::release_count));
   }
 };
 
