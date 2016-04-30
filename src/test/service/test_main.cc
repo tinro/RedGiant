28f062272491e3fc01c1116a7d538c9a7136fb3e
Tinro <tinrow@gmail.com>
2016-04-30 14:27:21 +0000

    fix failed test cases and fix the test return code

--- src/test/service/test_main.cc
+++ src/test/service/test_main.cc
@@ -12,6 +12,6 @@ int main( int argc, char **argv)
   CppUnit::TextUi::TestRunner runner;
   CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
   runner.addTest(registry.makeTest());
-  runner.run();
-  return 0;
+  // return 0 if successful
+  return (int)!runner.run();
 }
